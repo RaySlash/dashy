0c1fb652e2b59b6e9e136df71d6e143f5a6c5ad8
RaySlash <stevemathewjoy@gmail.com>
2024-04-04 15:46:12 +1000

    src: update page details

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
     <>
       <Head>
-        <title>Tamagui Example App</title>
+        <title>Dashy</title>
         <meta name="description" content="Tamagui, Solito, Expo & Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
